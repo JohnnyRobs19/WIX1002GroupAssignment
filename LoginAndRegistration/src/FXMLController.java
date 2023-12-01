fd3e2b06e8d2702be7c7e2509784d146cc2c0f59
Jonathan Siew <jonathansiew.2004@gmail.com>
2023-12-01 18:36:58 +0800

    Further Improvements on Sign Up Page
    1. Have added comments
    2. Added function to increment EmployeeId Properly when there's a new Entry
    3. Make sure there is no longer duplicate entry (an existing user cannot sign up again)

--- LoginAndRegistration/src/FXMLController.java
+++ LoginAndRegistration/src/FXMLController.java
@@ -25,77 +25,130 @@ public class FXMLController implements Initializable {
     private Label wrongSignUp;
     @FXML
     private Label correctSignUp;
-    
 
+    // File path for the CSV file
     private static final String CSV_FILE_PATH = "src//employee.csv";
-    
+
+    // Default constructor
     public FXMLController() {
         // Note: It's not recommended to initialize @FXML fields here
         // Instead, use the initialize method.
     }
 
+    // Initialization logic for the controller
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Initialization logic here, if needed
     }
-    
+
+    // Main method (not used in this context)
     public static void main(String[] args) {
-        //instatiating the CSVWriter Class
+        // Instantiating the CSVWriter Class could be done here
     }
 
-@FXML
-private void userSignUp(ActionEvent event) {
-    String enteredEmployeeName = employeeName.getText();
-    String enteredPassword = password.getText();
-    String enteredPassword2 = password2.getText();
+    // Event handler for the Sign Up button
+    @FXML
+    private void userSignUp(ActionEvent event) {
+        String enteredEmployeeName = employeeName.getText();
+        String enteredPassword = password.getText();
+        String enteredPassword2 = password2.getText();
 
-    if (!enteredPassword.equals(enteredPassword2)) {
-        wrongSignUp.setText("Your credentials are not consistent");
-        employeeName.clear();
-        password.clear();
-        password2.clear();
-    } else {
         try {
-            // Read the last employeeId from the existing CSV file
-            int lastEmployeeId = getLastEmployeeId();
+            // Check if the entered employeeName already exists in the CSV file
+            if (isEmployeeNameDuplicate(enteredEmployeeName)) {
+                wrongSignUp.setText("You have already signed up.");
+                clearFormFields();
+            // If Password and Re-enter Password Field are not the same
+            } else if (!enteredPassword.equals(enteredPassword2)) {
+                wrongSignUp.setText("Your credentials are not consistent");
+                clearFormFields();
+            } else {
+                // Read the last employeeId from the existing CSV file
+                int lastEmployeeId = getLastEmployeeId();
 
-            // Calculate the new employeeId
-            int newEmployeeId = lastEmployeeId + 1;
+                // Calculate the new employeeId
+                int newEmployeeId = lastEmployeeId + 1;
 
-            // Append the new employee data to the CSV file
-            appendEmployeeData(newEmployeeId, enteredEmployeeName, enteredPassword);
+                // Append the new employee data to the CSV file
+                appendEmployeeData(newEmployeeId, enteredEmployeeName, enteredPassword);
 
-            // Clear the form fields
-            employeeName.clear();
-            password.clear();
-            password2.clear();
-            correctSignUp.setText("You have successfully signed up.");
-            
+                // Clear the form fields
+                clearFormFields();
+                correctSignUp.setText("You have successfully signed up.");
+                wrongSignUp.setText("");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
+
+    // Event handler for the Clear button
     @FXML
     private void userClear(ActionEvent event) {
+        clearFormFields();
+        wrongSignUp.setText("");
+    }
+
+    // Function to determine the last EmployeeId from the CSV file
+    private int getLastEmployeeId() throws IOException {
+        try (CSVReader reader = new CSVReader(new FileReader(CSV_FILE_PATH))) {
+            List<String[]> lines = reader.readAll();
+
+            // If the list is not empty, find and return the correct numeric part of the last employeeId
+            if (!lines.isEmpty()) {
+                for (int i = lines.size() - 1; i >= 0; i--) {
+                    String lastEmployeeId = lines.get(i)[0];
+                    if (lastEmployeeId.startsWith("E")) {
+                        return extractNumericId(lastEmployeeId);
+                    }
+                }
+            }
+
+            // If the list is empty or no valid employeeId is found, return 0
+            return 0;
+        }
+    }
+
+    // Function to extract the numeric part of EmployeeId (format: E0001)
+    private int extractNumericId(String employeeId) {
+        // Extract and return the numeric part of the employeeId
+        return Integer.parseInt(employeeId.substring(1));
+    }
+
+    // Function to append Employee data to the CSV file
+    private void appendEmployeeData(int lastEmployeeId, String employeeName, String password) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(CSV_FILE_PATH, true))) {
+            // Generate the new employeeId by incrementing the last four digits
+            String newEmployeeId = "E" + String.format("%04d", (lastEmployeeId));
+
+            // Create an array with employee data
+            String[] data = {newEmployeeId, employeeName, "0", password};
+
+            // Write data to the CSV file
+            writer.writeNext(data);
+        }
+    }
+
+    // Function to check if the entered EmployeeName already exists in the CSV file
+    private boolean isEmployeeNameDuplicate(String enteredEmployeeName) throws IOException {
+        try (CSVReader reader = new CSVReader(new FileReader(CSV_FILE_PATH))) {
+            List<String[]> lines = reader.readAll();
+
+            for (String[] line : lines) {
+                String existingEmployeeName = line[1]; // Assuming employeeName is at index 1
+                if (enteredEmployeeName.trim().equalsIgnoreCase(existingEmployeeName.trim())) {
+                    return true; // Duplicate found
+                }
+            }
+
+            return false; // No duplicate found
+        }
+    }
+
+    // Helper method to clear form fields
+    private void clearFormFields() {
         employeeName.clear();
         password.clear();
         password2.clear();
-        wrongSignUp.setText("");
     }
-    
- private int getLastEmployeeId() throws IOException {
-    try (CSVReader reader = new CSVReader(new FileReader(CSV_FILE_PATH))) {
-        List<String[]> lines = reader.readAll();
-        // If the list is not empty, return the last employeeId
-        return lines.isEmpty() ? 0 : Integer.parseInt(lines.get(lines.size() - 1)[0]);
-    }
-}
-
-private void appendEmployeeData(int employeeId, String employeeName, String password) throws IOException {
-    try (CSVWriter writer = new CSVWriter(new FileWriter(CSV_FILE_PATH, true))) {
-        String[] data = {String.valueOf(employeeId), employeeName, "0", password};
-        writer.writeNext(data);
-    }
-}
 }
--- LoginAndRegistration/src/Testing.java
+++ LoginAndRegistration/src/Testing.java
@@ -14,34 +14,54 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
+ * The Testing class serves as the main entry point for the LeCars Sales Management System application.
+ * It extends the JavaFX Application class and provides the start method for initializing and displaying the GUI.
  *
  * @author zxsie
  */
+
+// Place to run the interface
 public class Testing extends Application {
     
+    /**
+     * The start method is called when the JavaFX application is launched.
+     * It loads the FXML file, sets up the main stage, and displays the user interface.
+     *
+     * @param primaryStage The primary stage for the JavaFX application.
+     */
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root=FXMLLoader.load(getClass().getResource("FXML.fxml"));
+            // Load the FXML file to create the UI layout
+            Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+            
+            // Create a scene using the UI layout
             Scene scene = new Scene(root);
             
+            // Set the title of the main stage
             primaryStage.setTitle("LeCars Sales Management System");
+            
+            // Set the scene for the primary stage
             primaryStage.setScene(scene);
             
-            //Disable window resizing (including maximizing)
+            // Disable window resizing (including maximizing)
             primaryStage.setResizable(false);
             
+            // Show the primary stage
             primaryStage.show();
         } catch (IOException ex) {
+            // Handle exceptions that may occur during FXML file loading
             Logger.getLogger(Testing.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     /**
-     * @param args the command line arguments
+     * The main method is the entry point for the Java application.
+     * It launches the JavaFX application by calling the launch method.
+     *
+     * @param args The command line arguments passed to the application.
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
